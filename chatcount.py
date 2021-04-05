1d45079bac3bd7bf327f6c756c1098e451570a7d
zxc157 <50169382+zxc157@users.noreply.github.com>
2021-04-05 23:14:42 +0900

    Add files via upload

--- chatcount.py
+++ chatcount.py
@@ -48,7 +48,7 @@ def countchat2(f_name,sec):
     lines=f.readlines()
     countline=len(lines)
     print(countline)
-    print(lines[-1])
+    #print(lines[-1])
     totaltime_h=int(lines[-1][1])*10+int(lines[-1][2])
     totaltime_m=int(lines[-1][4])*10+int(lines[-1][5])
     totaltime_s=int(lines[-1][7])*10+int(lines[-1][8])
@@ -90,6 +90,63 @@ def countchat2(f_name,sec):
     f.close()
     return highlights
 
+
+def countchat3(f_name,sec):
+    filename=f_name
+    f=open(str(f_name)+".txt",'rt',encoding="utf-8")
+    lines=f.readlines()
+    countline=len(lines)
+    print(countline)
+    print(lines[-1])
+    totaltime_h=int(lines[-1][1])*10+int(lines[-1][2])
+    totaltime_m=int(lines[-1][4])*10+int(lines[-1][5])
+    totaltime_s=int(lines[-1][7])*10+int(lines[-1][8])
+    totalsec=3600*totaltime_h+60*totaltime_m+totaltime_s
+    chattable=[]
+    highlights=[]
+    avgchat=(countline/int(totalsec))*int(sec)
+    dic=['ㅋ','오','와','ㅜ','ㅠ','?']
+    wordcount=0
+    countlen=0
+    print(avgchat)
+    for line in lines:
+        timesec=(int(line[1])*10+int(line[2]))*3600+(int(line[4])*10+int(line[5]))*60+int(line[7])*10+int(line[8])
+        chattable.append(timesec)
+        text=line[11:-1]
+        textlen=len(text)
+        countlen+=textlen
+        textword=text.split(" ")
+        for t in textword:
+            if t in dic:
+                wordcount+=1
+                break
+        for i in chattable:
+            if timesec > i+sec :
+                chattable.remove(i)
+
+        if ( len(chattable) >= avgchat*3.5) :
+            if (timesec not in highlights):
+                highlights.append(timesec)
+        
+    checkcount=0
+    checknum=0
+    avglen=countlen/countline
+    print(avglen)
+    print(wordcount)
+    result=[]
+    #print(highlights)
+    for k in highlights:
+        for i in range(k+1,k+30):
+            if i in highlights:
+                highlights.remove(i)
+            
+    
+
+    print(highlights)
+    
+    f.close()
+    return highlights
+
 def preprocessing(string):
     prechar=""
     for i in string:
@@ -112,6 +169,8 @@ def analyzeh(f_name,seclist,sec):
     pasttime=0
     countlen=0
     countline=0
+    countword=0
+    dic=['ㅋ','오','와','ㅜ','ㅠ','?']
     for line in lines:
         
         timesec=(int(line[1])*10+int(line[2]))*3600+(int(line[4])*10+int(line[5]))*60+int(line[7])*10+int(line[8])
@@ -131,6 +190,10 @@ def analyzeh(f_name,seclist,sec):
 
                 else:
                     word_list[word2] = 1
+            for t in textword:
+                if t in dic:
+                    countword+=1
+                    break
             if timesec in seclist:
                 pasttime=timesec
                 timecheck+=1
@@ -140,7 +203,7 @@ def analyzeh(f_name,seclist,sec):
                     pasttime=timesec
 
         if timecheck >= sec :
-            print(timesec)
+            #print(timesec)
             word_list=sorted(word_list.items(), key=lambda x:x[1], reverse=True)
             #print(word_list)
             timecheck=0
@@ -150,9 +213,11 @@ def analyzeh(f_name,seclist,sec):
         
     avglen=countlen/countline
     print(avglen)
+    print(countword)
+    print(countline)
     f.close()
                 
 if __name__ == "__main__":
-    highlist=countchat2(877099804,30)
+    highlist=countchat3(877099804,30)
     analyzeh(877099804,highlist,30)
     
